1a55733c252e9684bdd5dde251c5a8afeb4cb03a
johnny-vonny <shauchuk.ihar@gmail.com>
2019-03-24 22:50:12 +0300

    1

---
+++ hw_04/task_02.py
@@ -0,0 +1,11 @@
+# 2) Дан список целых чисел. Подсчитать сколько четных чисел в списке
+
+list_1 = [1, 2, 3, 4, 5]
+i = 0
+count = 0
+lenght = len(list_1)
+while i < lenght:
+    if list_1[i] % 2 == 0:
+        count += 1
+    i += 1
+print (count)
